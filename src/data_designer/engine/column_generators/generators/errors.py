170f71652a8edec6a38bb87ee8e726f7094574e7
Johnny Greco <jogreco@nvidia.com>
2025-11-18 00:34:04 -0500

    remove unused error files

--- src/data_designer/engine/column_generators/generators/errors.py
+++
@@ -1,9 +0,0 @@
-from data_designer.errors import DataDesignerError
-
-
-class MissingResourceError(DataDesignerError):
-    """Exception for all errors related to missing resources."""
-
-
-class MissingBlobStorageError(MissingResourceError):
-    """Exception for all errors related to missing blob storage."""
--- src/data_designer/engine/resources/errors.py
+++
@@ -1,9 +0,0 @@
-from data_designer.errors import DataDesignerError
-
-
-class MissingBlobStorageError(DataDesignerError):
-    """Exception for all errors related to missing blob storage."""
-
-
-class MissingManagedAssetsError(DataDesignerError):
-    """Exception for all errors related to missing managed assets."""
